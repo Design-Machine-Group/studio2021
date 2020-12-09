42550e5e5ea4b2465501b81b897d4a6393a1e543
preston pape <papepreston@gmail.com>
2020-12-08 20:22:55 -0800

    working version of material reader script and creation of materials.csv

--- src/studio2021/functions/material_reader.py
+++ src/studio2021/functions/material_reader.py
@@ -32,7 +32,8 @@ import studio2021
 # ---------------------------------------------------------------------------------------------------------------
 
 
-def read_materials(filepath):
+def read_materials(material_name):
+    filepath = os.path.join(studio2021.DATA, 'materials.csv')
     fh = open(filepath, 'r')
     lines = fh.readlines()
     fh.close()
@@ -48,19 +49,11 @@ def read_materials(filepath):
                 i = float(i)
         materials[material] = {'type': type, 'r_value': r_value, 'r_per_in': r_per_in, 'thickness_in': thickness_in,
                                'thickness_m': thickness_m, 'conductivity': conductivity, 'density': density, 'sp_heat': sp_heat, 'embodied_carbon': embodied_carbon}
-    return materials
+    return materials[material_name]
 
 
 if __name__ == "__main__":
     for i in range(50):
         print()
-    folder = studio2021.DATA
-    filepath = os.path.join(folder, 'materials.csv')
-    materials = read_materials(filepath)
-
-    print(materials)
-
-    for key in materials:
-        # print(key, type)
-        for embodied_carbon in materials[key]:
-            print(key, materials[key][embodied_carbon])
+    material = read_materials("Plywood")
+    print(material)
