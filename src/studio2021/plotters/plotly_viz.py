db095f10d559405bf01ecfc50dd578681fbddfd6
preston pape <papepreston@gmail.com>
2021-02-07 11:24:39 -0800

    created plotly_viz.py

--- src/studio2021/plotters/plotly_viz.py
+++ src/studio2021/plotters/plotly_viz.py
@@ -5,12 +5,19 @@ import studio2021
 
 
 def import_data(path):
+    """
+    Imports .csv data files for visualization.
+    Currently only brings in one .csv- this will be expanded to loop, which will search directory for all .csv
+    """
     fh = open(path, 'r')
     df = pd.read_csv(fh)
     return df
 
 
-def plot_data(data):
+def plot_data(x_axis, y_axis):
+    """
+    Plots selected data on x and y axes from pandas dataframe.
+    """
     fig = px.scatter()
     fig.show()
     return
