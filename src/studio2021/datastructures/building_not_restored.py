60d9f7f8e107bb2c2b7715cf3883fe6e742bb60b
Tomas Mendez Echenagucia <tmendeze@uw.edu>
2021-02-25 10:32:33 -0800

    restoring deleted file

--- src/studio2021/datastructures/building_not_restored.py
+++ src/studio2021/datastructures/building_not_restored.py
@@ -37,8 +37,8 @@ class Building(object):
         self.__name__               = 'Studio2021Building'
         self.orient_dict            = {0:'n', 1:'s', 2:'e', 3:'w'}
         self.zones                  = {}
-        self.floor_surfaces         = {}
         self.ceiling_surfaces       = {}
+        self.floor_surfaces         = {}
         self.adiabatic_walls        = {}
         self.facade_areas           = {'n':{}, 's':{}, 'e':{}, 'w':{}}
         self.window_areas           = {'n':{}, 's':{}, 'e':{}, 'w':{}}
@@ -249,13 +249,13 @@ class Building(object):
         import rhinoscriptsyntax as rs
 
         znames                  = data['znames']
+        ceiling_surfaces        = data['ceiling_surfaces']
         exterior_walls_n        = data['exterior_walls_n']
         exterior_walls_s        = data['exterior_walls_s']
         exterior_walls_e        = data['exterior_walls_e']
         exterior_walls_w        = data['exterior_walls_w']
         adiabatic_walls         = data['adiabatic_walls']
         floor_surfaces          = data['floor_surfaces']
-        ceiling_surfaces        = data['ceiling_surfaces']
         wwr                     = data['wwr']
         shade_gc                = data['shade_gc']
         shade_depth             = data['shade_depth']
@@ -278,13 +278,24 @@ class Building(object):
         num_zones = znames.BranchCount
         b.zones = {i: znames.Branch(i)[0] for i in range(num_zones)}
 
+        # ceiling - - -
+        for i in range(ceiling_surfaces.BranchCount):
+            srf = ceiling_surfaces.Branch(i)
+            if len(srf) > 0:
+                p = rs.SurfacePoints(srf[0])
+                # p = [p[0], p[2], p[3], p[1]]
+                p = [list(p[j]) for j in [0, 2, 3, 1]]
+                b.ceiling_surfaces[b.zones[i]] = p
+
         # exterior walls - - -
         # north - 
         for i in b.zones:
             if i < exterior_walls_n.BranchCount:
                 srf = exterior_walls_n.Branch(i)
                 if len(srf) > 0:
-                    p = rs.PolylineVertices(srf[0])
+                    p = rs.SurfacePoints(srf[0])
+                    # p = [p[0], p[2], p[3], p[1]]
+                    p = [list(p[j]) for j in [0, 2, 3, 1]]
                     b.exterior_walls['n'][b.zones[i]] = p
 
         # south - 
@@ -292,7 +303,9 @@ class Building(object):
             if i < exterior_walls_s.BranchCount:
                 srf = exterior_walls_s.Branch(i)
                 if len(srf) > 0:
-                    p = rs.PolylineVertices(srf[0])
+                    p = rs.SurfacePoints(srf[0])
+                    # p = [p[0], p[2], p[3], p[1]]
+                    p = [list(p[j]) for j in [0, 2, 3, 1]]
                     b.exterior_walls['s'][b.zones[i]] = p
 
         # east - 
@@ -300,7 +313,9 @@ class Building(object):
             if i < exterior_walls_e.BranchCount:
                 srf = exterior_walls_e.Branch(i)
                 if len(srf) > 0:
-                    p = rs.PolylineVertices(srf[0])
+                    p = rs.SurfacePoints(srf[0])
+                    # p = [p[0], p[2], p[3], p[1]]
+                    p = [list(p[j]) for j in [0, 2, 3, 1]]
                     b.exterior_walls['e'][b.zones[i]] = p
 
         # west - 
@@ -308,30 +323,29 @@ class Building(object):
             if i < exterior_walls_w.BranchCount:
                 srf = exterior_walls_w.Branch(i)
                 if len(srf) > 0:
-                    p = rs.PolylineVertices(srf[0])
+                    p = rs.SurfacePoints(srf[0])
+                    # p = [p[0], p[2], p[3], p[1]]
+                    p = [list(p[j]) for j in [0, 2, 3, 1]]
                     b.exterior_walls['w'][b.zones[i]] = p
 
         # adiabatic walls - - -
-        for k in range(adiabatic_walls.BranchCount):
-            walls = adiabatic_walls.Branch(k)
-            b.adiabatic_walls[b.zones[k]] = []
-            for wall in walls:
-                p = rs.PolylineVertices(wall)
-                b.adiabatic_walls[b.zones[k]].append(p)
+        for i in range(adiabatic_walls.BranchCount):
+            srfs = adiabatic_walls.Branch(i)
+            b.adiabatic_walls[b.zones[i]] = []
+            for srf in srfs:
+                p = rs.SurfacePoints(srf)
+                # p = [p[0], p[2], p[3], p[1]]
+                p = [list(p[j]) for j in [0, 2, 3, 1]]
+                b.adiabatic_walls[b.zones[i]].append(p)
 
         # floor surfaces - - -
         for i in range(floor_surfaces.BranchCount):
             srf = floor_surfaces.Branch(i)
             if len(srf) > 0:
-                p = rs.PolylineVertices(srf[0])
+                p = rs.SurfacePoints(srf[0])
+                # p = [p[0], p[2], p[3], p[1]]
+                p = [list(p[j]) for j in [0, 2, 3, 1]]
                 b.floor_surfaces[b.zones[i]] = p
-
-        # ceiling surfaces - - -
-        for i in range(ceiling_surfaces.BranchCount):
-            srf = ceiling_surfaces.Branch(i)
-            if len(srf) > 0:
-                p = rs.PolylineVertices(srf[0])
-                b.ceiling_surfaces[b.zones[i]] = p
 
         # wwr - - -
         for i in range(wwr.BranchCount):
@@ -401,8 +415,8 @@ class Building(object):
                 adiabatic = self.adiabatic_walls[zkey]
                 for srf in adiabatic:
                     srfs.append(srf)
-            # if zkey in self.ceiling_surfaces:
-            #     srfs.append(self.ceiling_surfaces[zkey])
+            if zkey in self.ceiling_surfaces:
+                srfs.append(self.ceiling_surfaces[zkey])
             if zkey in self.floor_surfaces:
                 srfs.append(self.floor_surfaces[zkey])
             for okey in self.exterior_walls:
@@ -457,8 +471,7 @@ class Building(object):
             okey = self.orient_dict[i]
             walls = [[] for _ in range(len(self.exterior_walls[okey]))]
             for i, zkey in enumerate(self.exterior_walls[okey]):
-                pl = rs.AddPolyline(self.exterior_walls[okey][zkey])
-                walls[i] = [rs.AddPlanarSrf(pl)[0]]
+                walls[i] = [rs.AddSrfPt(self.exterior_walls[okey][zkey])]
             data['exterior_walls'][okey] = th.list_to_tree(walls, source=[])
 
         # cieling - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -466,8 +479,7 @@ class Building(object):
         ceiling_surfaces = [[] for _ in range(len(zones))]
         for i, zone in enumerate(zones):
             if zone in self.ceiling_surfaces:
-                pl = rs.AddPolyline(self.ceiling_surfaces[zone])
-                ceiling_surfaces[i].append(rs.AddPlanarSrf(pl)[0])
+                ceiling_surfaces[i].append(rs.AddSrfPt(self.ceiling_surfaces[zone]))
         data['ceiling_surfaces'] = th.list_to_tree(ceiling_surfaces, source=[])
 
         # floor_surfaces - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -475,8 +487,7 @@ class Building(object):
         floor_surfaces = [[] for _ in range(len(zones))]
         for i, zone in enumerate(zones):
             if zone in self.floor_surfaces:
-                pl = rs.AddPolyline(self.floor_surfaces[zone])
-                floor_surfaces[i].append(rs.AddPlanarSrf(pl)[0])
+                floor_surfaces[i].append(rs.AddSrfPt(self.floor_surfaces[zone]))
         data['floor_surfaces'] = th.list_to_tree(floor_surfaces, source=[])
 
         # adiabatic_walls - - - - - - - - - - - - - - - - - - - - - - - - - - - 
@@ -486,8 +497,7 @@ class Building(object):
             if zone in self.adiabatic_walls:
                 srfs = self.adiabatic_walls[zone]
                 for srf in srfs:
-                    pl = rs.AddPolyline(srf)
-                    adiabatic_walls[i].append(rs.AddPlanarSrf(pl)[0])
+                    adiabatic_walls[i].append(rs.AddSrfPt(srf))
         data['adiabatic_walls'] = th.list_to_tree(adiabatic_walls, source=[])
 
         # wwr - - - - - - - - - - - - - - - - - - - - - - - - - - -
